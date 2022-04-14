6922bbc422f2dc1747c099c68d23bb147df60ff8
ldex <laurent.duveau+git@gmail.com>
2022-04-14 10:24:41 -0400

    Progressive Pagination

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { catchError, delay, first, map, mergeAll, shareReplay, tap } from 'rxjs/operators';
 import { Product } from './product.interface';
 
@@ -10,8 +10,13 @@ import { Product } from './product.interface';
 export class ProductService {
 
   private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
-  products$: Observable<Product[]>;
+
+  private productsSubject = new BehaviorSubject<Product[]>([]);
+  products$: Observable<Product[]> = this.productsSubject.asObservable();
+
   mostExpensiveProduct$: Observable<Product>;
+
+  productsToLoad = 10;
 
   constructor(private http: HttpClient) {
     this.initProducts();
@@ -31,17 +36,26 @@ export class ProductService {
       )
   }
 
-  initProducts() {
-    let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
+  initProducts(skip: number = 0, take: number = this.productsToLoad) {
+    let url = this.baseUrl + `?$skip=${skip}&$top=${take}&$orderby=ModifiedDate%20desc`;
 
-    this.products$ = this
-                      .http
-                      .get<Product[]>(url)
-                      .pipe(
-                        delay(1500), // demo only!
-                        tap(console.table),
-                        shareReplay()
-                      );
+    this
+      .http
+      .get<Product[]>(url)
+      .pipe(
+        delay(1500), // demo only!
+        tap(console.table),
+        shareReplay(),
+        map(
+          newProducts => {
+            let currentProducts = this.productsSubject.value;
+            return currentProducts.concat(newProducts);
+          }
+        )
+      )
+      .subscribe(
+        fullList => this.productsSubject.next(fullList)
+      );
   }
 
   insertProduct(newProduct: Product): Observable<Product> {
